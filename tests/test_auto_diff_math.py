bbc99c33d5f985c9f2c2fe1ba28a1e823e655922
Dan Nolte <dnolte@hsph.harvard.edu>
2022-11-16 10:19:32 -0500

    add tests for hypernolic trig functions

--- tests/test_auto_diff_math.py
+++ tests/test_auto_diff_math.py
@@ -10,64 +10,109 @@ import numpy as np
 # import names to test
 from autodiff.utils.auto_diff_math import *
 
+
 class TestAutoDiffMath:
     """Test class for dual number types"""
     def test_sin(self):
-        z1 = DualNumber(np.pi/4)
-        z2 = np.pi/4
+        z1 = DualNumber(np.pi / 4)
+        z2 = np.pi / 4
 
-        assert sin(z1).real == math.sin(np.pi/4)
-        assert sin(z1).dual == math.cos(np.pi/4)
-        
-        assert sin(z2).real == math.sin(np.pi/4)
+        assert sin(z1).real == math.sin(np.pi / 4)
+        assert sin(z1).dual == math.cos(np.pi / 4)
+
+        assert sin(z2).real == math.sin(np.pi / 4)
         assert sin(z2).dual == 0
 
+        with pytest.raises(TypeError):
+            sin("string")
+
     def test_cos(self):
-        z3 = DualNumber(np.pi)
-        z4 = np.pi
+        z1 = DualNumber(np.pi)
+        z2 = np.pi
 
-        assert cos(z3).real == math.cos(np.pi)
-        assert cos(z3).dual == -math.sin(np.pi)
+        assert cos(z1).real == math.cos(np.pi)
+        assert cos(z1).dual == -math.sin(np.pi)
 
-        assert cos(z4).real == math.cos(np.pi)
-        assert cos(z4).dual == 0
-        
+        assert cos(z2).real == math.cos(np.pi)
+        assert cos(z2).dual == 0
+
+        with pytest.raises(TypeError):
+            cos("string")
 
     def test_tan(self):
-        z5 = DualNumber(np.pi)
-        z6 = np.pi
+        z1 = DualNumber(np.pi)
+        z2 = np.pi
 
-        assert tan(z5).real == math.tan(np.pi)
-        assert tan(z5).dual == 1/math.cos(np.pi) ** 2
+        assert tan(z1).real == math.tan(np.pi)
+        assert tan(z1).dual == 1 / math.cos(np.pi)**2
 
-        assert tan(z6).real == math.tan(np.pi)
-        assert tan(z6).dual == 0
+        assert tan(z2).real == math.tan(np.pi)
+        assert tan(z2).dual == 0
+
+        with pytest.raises(TypeError):
+            tan("string")
 
     def test_exp(self):
-        z7 = DualNumber(1)
-        z8 = 1
+        z1 = DualNumber(1)
+        z2 = 1
 
-        assert exp(z7).real == np.e
-        assert exp(z7).dual == np.e
+        assert exp(z1).real == np.e
+        assert exp(z1).dual == np.e
 
-        assert exp(z8).real == np.e
-        assert exp(z8).dual == 0
+        assert exp(z2).real == np.e
+        assert exp(z2).dual == 0
+
+        with pytest.raises(TypeError):
+            exp("string")
 
     def test_log(self):
-        z9 = DualNumber(5)
-        z10 = 5
+        z1 = DualNumber(5)
+        z2 = 5
 
-        assert log(z9).real == np.log(5)
-        assert log(z9).dual == 1/5
+        assert log(z1).real == np.log(5)
+        assert log(z1).dual == 1 / 5
 
-        assert log(z10).real == np.log(5)
-        assert log(z10).dual == 0
+        assert log(z2).real == np.log(5)
+        assert log(z2).dual == 0
+
+        with pytest.raises(TypeError):
+            log("string")
 
     def test_sinh(self):
-        pass
+        z1 = DualNumber(5)
+        z2 = 5
+
+        assert sinh(z1).real == np.sinh(5)
+        assert sinh(z1).dual == np.cosh(5)
+
+        assert sinh(z2).real == np.sinh(5)
+        assert sinh(z2).dual == 0
+
+        with pytest.raises(TypeError):
+            sinh("string")
 
     def test_cosh(self):
-        pass
+        z1 = DualNumber(5)
+        z2 = 5
+
+        assert cosh(z1).real == np.cosh(5)
+        assert cosh(z1).dual == np.sinh(5)
+
+        assert cosh(z2).real == np.cosh(5)
+        assert cosh(z2).dual == 0
+
+        with pytest.raises(TypeError):
+            cosh("string")
 
     def test_tanh(self):
-        pass
+        z1 = DualNumber(0)
+        z2 = 0
+
+        assert tanh(z1).real == np.tanh(0)
+        assert tanh(z1).dual == 1 - np.tanh(0)**2
+
+        assert tanh(z2).real == np.tanh(0)
+        assert tanh(z2).dual == 0
+
+        with pytest.raises(TypeError):
+            tanh("string")
