516a0376c09edf787494ed6cbf0c8f0d79086d6c
dah0043 <danielherrera@hsph.harvard.edu>
2022-11-19 00:41:58 -0500

    add dual number class tests except power/reflective power

--- tests/test_dual_numbers.py
+++ tests/test_dual_numbers.py
@@ -5,6 +5,8 @@ autodiff package.
 
 import pytest
 
+import numpy as np
+
 # import names to test
 from autodiff.utils.dual_numbers import DualNumber
 
@@ -41,11 +43,11 @@ class TestDualNumber:
     def test_reflective_addition(self):
         z1 = DualNumber(1, 2)
 
-        # DualNumber + int
+        # int + DualNumber
         assert (z1 + 3).real == (3 + z1).real == 4
         assert (z1 + 3).dual == (3 + z1).dual == 2
 
-        # DualNumber + float
+        # float + DualNumber
         assert (z1 + 3.0).real == (3.0 + z1).real == 4.0
         assert (z1 + 3.0).dual == (3.0 + z1).dual == 2.0
 
@@ -73,7 +75,15 @@ class TestDualNumber:
             "string" - z1
 
     def test_reflective_subtraction(self):
-        pass
+        z1 = DualNumber(5, 5)
+
+        # int - DualNumber 
+        assert (1 - z1).real == -4
+        assert (1 - z1).dual == -5
+
+        # float - DualNumber
+        assert (1.0 - z1).real == -4.0
+        assert (1.0 - z1).dual == -5.0
 
     def test_multiplication(self):
         z1 = DualNumber(1, 2)
@@ -98,37 +108,108 @@ class TestDualNumber:
             z1 * "string"
             "string" * z1
 
-    def test_reflexive_multiplication(self):
+    def test_reflective_multiplication(self):
         z1 = DualNumber(1, 2)
 
-        # DualNumber * int
+        # int * DualNumber
         assert (z1 * 3).real == (3 * z1).real == 3
         assert (z1 * 3).dual == (3 * z1).dual == 6
 
-        # DualNumber * float
+        # float * DualNumber
         assert (z1 * 3.0).real == (3.0 * z1).real == 3.0
         assert (z1 * 3.0).dual == (3.0 * z1).dual == 6.0
+    
+    def test_true_division(self):
+        z1 = DualNumber(5, 5)
+        z2 = DualNumber(1, 1)
 
-    def test_reflexive_true_division(self):
-        pass
+        # DualNumber / DualNumber
+        assert (z1 / z2).real == 5
+        assert (z1 / z2).dual == 0
+
+        # DualNumber / int
+        assert (z1 / 5).real == 1
+        assert (z1 / 5).dual == 1
+
+        # DualNumber / float
+        assert (z1 / 5.0).real == 1.0
+        assert (z1 / 5.0).dual == 1.0
+
+        # Handle Non-Supported Types (String)
+        with pytest.raises(TypeError):
+            z1 / "string"
+            "string" / z1
+
+    def test_reflective_true_division(self):
+        z1 = DualNumber(5, 5)
+
+        # int / DualNumber 
+        assert (5 / z1).real == 1
+        assert (5 / z1).dual == -1
+
+        # float / DualNumber
+        assert (5.0 / z1).real == 1.0
+        assert (5.0 / z1).dual == -1.0
 
     def test_power(self):
-        pass
+        z1 = DualNumber(5, 5)
 
-    def test_reflexive_power(self):
+        # DualNumber ** int
+        assert (z1 ** 3).real == 125
+        assert (z1 ** 3).dual == 375
+
+        # DualNumber ** float
+        assert (z1 ** 3.0).real == 125.0
+        assert (z1 ** 3.0).dual == 375.0
+
+        # Handle Non-Supported Types (String)
+        with pytest.raises(TypeError):
+            z1 ** "string"
+            "string" ** z1
+
+        # # Dual Number ** Dual Number
+        # z2 = DualNumber(1, 1)
+        # assert (z1 ** z2).real == 5
+        # assert (z1 ** z2).dual == (5 ** 0) * (1 * 5 + np.log(5) * 5 * 1)
+
+    def test_reflective_power(self):
         pass
 
     def test_negation(self):
-        pass
+        z1 = DualNumber(5, 5)
+         # -DualNumber
+        assert (-z1).real == -5
+        assert (-z1).dual == -5
 
     def test_repr(self):
-        pass
+        z1 = DualNumber(5, 5)
+        # repr(DualNumber)
+        assert repr(z1) == 'DualNumber(5, 5)'
 
     def test_equal(self):
-        pass
+        z1 = DualNumber(2, 2)
+        z2 = DualNumber(2, 1)
+        z3 = DualNumber(2, 2)
 
-    def test_reflective_multiplication(self):
-        pass
+        # DualNumber == DualNumber
+        assert z1 == z3
+        assert not (z1 == z2)
+
+        # Handle Int/Float Comparisons
+        with pytest.raises(TypeError):
+            z1 == 2
+            z1 == 2.0
 
     def test_not_equal(self):
-        pass
+        z1 = DualNumber(2, 2)
+        z2 = DualNumber(2, 1)
+        z3 = DualNumber(2, 2)
+
+        # DualNumber != DualNumber
+        assert z1 != z2
+        assert not (z1 != z3)
+
+        # Handle Int/Float Comparisons
+        with pytest.raises(TypeError):
+            z1 != 2
+            z1 != 2.0
