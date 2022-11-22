762306ce65f5764d2ca63e4327b46030bb2e0e50
dah0043 <danielherrera@hsph.harvard.edu>
2022-11-22 01:12:20 -0500

    working newtons method driver script with get_value get_derivative

--- driver_script.py
+++ driver_script.py
@@ -1,16 +1,16 @@
-"""In this script we implement the newton method using automatic differentiation"""
 #!/usr/bin/env python
+"""In this script we implement the newton method using automatic differentiation"""
 import numpy as np
 from autodiff.auto_diff import AutoDiff
 from autodiff.utils.auto_diff_math import *
 
 f = lambda x: x**2 - 5 * x + 2 * exp(x) - sin(x) - 4
-x_k = 1.5
+x_k = 0.5
 tol = 1e-4
 max_it = 1000
 for k in range(max_it):
-    der = AutoDiff(f).get_derivative(x_k, 1)[0] # will not need [0] once get_derivative R --> R fix
-    val = f(x_k).real # will change to autoDiff.get_value instead of f(x_k.real)
+    der = AutoDiff(f).get_derivative(x_k, 1)
+    val = AutoDiff(f).get_value(x_k)
     dx_k = -val / der
     if abs(dx_k) < tol:
         root = x_k + dx_k
